36d4ef830615cf92db827d1bb70b4b37277e233b
SladenT <davisteigeler@gmail.com>
2024-12-20 19:32:29 -0500

    Basic game fully fuctional, only missing main menu and scoring, and harder levels

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "raylib.h"
 #include "types.h"
 #include "rng.h"
@@ -6,7 +7,7 @@
 // Variables //////////////////////////////////////
 ///////////////////////////////////////////////////
 TetroGridBlock grid[GRIDYMAX*GRIDXMAX];
-// Tetromino tetrominos[GRIDYMAX*GRIDXMAX]; // Can probably make this smaller by dividing by 4
+uInt deletedLines[4]; // Used for deletion and scoring
 uShort tetroBlockCnt = 0;
 uInt gridOffsetX = 224;
 uInt gridOffsetY = 32;
@@ -16,9 +17,43 @@ Tetromino controlledTetromino;
 GameState state = PLAYING;
 
 ///////////////////////////////////////////////////
+// Rotational Matrices ////////////////////////////
+///////////////////////////////////////////////////
+
+Vector2Int LRotMat[5][5] = 
+{
+    {{0,0},{0,0},{-2, 2},{ 0, 0},{ 0, 0}},
+    {{0,0},{0,2},{-1, 1},{-2, 0},{ 0, 0}},
+    {{2,2},{1,1},{ 0, 0},{-1,-1},{-2,-2}},
+    {{0,0},{2,0},{ 1,-1},{ 0,-2},{ 0, 0}},
+    {{0,0},{0,0},{ 2,-2},{ 0, 0},{ 0, 0}}
+};
+Vector2Int RRotMat[5][5] = 
+{
+    {{0, 0},{0, 0},{ 2, 2},{ 0, 0},{ 0, 0}},
+    {{0, 0},{2, 0},{ 1, 1},{ 0, 2},{ 0, 0}},
+    {{2,-2},{1,-1},{ 0, 0},{-1, 1},{-2, 2}},
+    {{0, 0},{0,-2},{-1,-1},{-2, 0},{ 0, 0}},
+    {{0, 0},{0, 0},{-2,-2},{ 0, 0},{ 0, 0}}
+};
+
+///////////////////////////////////////////////////
 // Tetromino Prototypes ///////////////////////////
 ///////////////////////////////////////////////////
-Tetromino box = (Tetromino){{{GRIDXMAX/2, 0}, {GRIDXMAX/2, 1}, {(GRIDXMAX/2)+1, 0}, {(GRIDXMAX/2)+1, 1}}, Box};
+Tetromino box    = (Tetromino){{{GRIDXMAX/2, 0}, {GRIDXMAX/2, 1}, {(GRIDXMAX/2)+1, 0}, {(GRIDXMAX/2)+1, 1}}, Box};
+Tetromino line   = (Tetromino){{{GRIDXMAX/2, 1}, {GRIDXMAX/2, 0}, {(GRIDXMAX/2), 2}, {(GRIDXMAX/2), 3}}, Line};
+Tetromino lr     = (Tetromino){{{GRIDXMAX/2, 1}, {GRIDXMAX/2, 0}, {(GRIDXMAX/2)+1, 0}, {(GRIDXMAX/2), 2}}, LR};
+Tetromino ll     = (Tetromino){{{GRIDXMAX/2, 1}, {GRIDXMAX/2, 0}, {(GRIDXMAX/2)-1, 0}, {(GRIDXMAX/2), 2}}, LL};
+Tetromino zr     = (Tetromino){{{GRIDXMAX/2, 1}, {GRIDXMAX/2, 0}, {(GRIDXMAX/2)+1, 0}, {(GRIDXMAX/2)-1, 1}}, ZR};
+Tetromino zl     = (Tetromino){{{GRIDXMAX/2, 1}, {GRIDXMAX/2, 0}, {(GRIDXMAX/2)-1, 0}, {(GRIDXMAX/2)+1, 1}}, ZL};
+Tetromino tblock = (Tetromino){{{GRIDXMAX/2, 1}, {GRIDXMAX/2, 0}, {(GRIDXMAX/2)-1, 0}, {(GRIDXMAX/2)+1, 0}}, T};
+
+
+
+///////////////////////////////////////////////////
+// Function Predeclarations ///////////////////////
+///////////////////////////////////////////////////
+static void CheckLineDelete();
 
 ///////////////////////////////////////////////////
 // Static Functions ///////////////////////////////
@@ -65,9 +100,12 @@ static void AddTetrominoToGrid(void)
 
 static void SpawnTetromino(void)
 {
+    
+    // Check if the previous drop scored points
+    CheckLineDelete();
     // Get tetromino type
-    controlledTetromino.type = (TetrominoType)GetNextInt(6);
-    controlledTetromino.type = 0;
+    controlledTetromino.type = (TetrominoType)GetNextInt(7);
+    // controlledTetromino.type = 0;
     // Fill out the Tetromino based upon the type
     switch(controlledTetromino.type)
     {
@@ -75,16 +113,22 @@ static void SpawnTetromino(void)
             controlledTetromino = box;
             break;
         case Line:
+            controlledTetromino = line;
             break;
         case LR:
+            controlledTetromino = lr;
             break;
         case LL:
+            controlledTetromino = ll;
             break;
         case ZR:
+            controlledTetromino = zr;
             break;
         case ZL:
+            controlledTetromino = zl;
             break;
         case T:
+            controlledTetromino = tblock;
             break;
     }
     // Check for valid spawn location, game over if none
@@ -95,7 +139,200 @@ static void SpawnTetromino(void)
         return;
     }
     // Do game over
+    state = GAME_OVER;
+    
+}
 
+static void GameUpdate()
+{
+    RemoveTetrominoFromGrid();
+    MoveTetrominoRelative(0, 1);
+    if (CheckValidTetromino())
+    {
+        AddTetrominoToGrid();
+    }
+    else
+    {
+        MoveTetrominoRelative(0, -1);
+        // Put down block and spawn new Tetromino
+        AddTetrominoToGrid();
+        SpawnTetromino();
+    }
+            tickCount = 0;
+}
+
+static bool AttemptMoveTetromino(int x, int y)
+{
+    RemoveTetrominoFromGrid();
+    MoveTetrominoRelative(x, y);
+    if (CheckValidTetromino())
+    {
+        AddTetrominoToGrid();
+        return true;
+    }
+    else
+    {
+        MoveTetrominoRelative(-x, -y);
+        AddTetrominoToGrid();
+        return false;
+    }
+}
+
+
+static void RotateTetromino(bool leftRight)
+{
+    // Create Rotational Matrix;
+    Vector2Int rotMat[5][5];
+    // Get values
+    Vector2Int stable = controlledTetromino.blocks[0];
+    Vector2Int one    = controlledTetromino.blocks[1];
+    Vector2Int two    = controlledTetromino.blocks[2];
+    Vector2Int three  = controlledTetromino.blocks[3];
+    // Modulate around stable block
+    one   = (Vector2Int){(one.x+2)-stable.x, (one.y+2)-stable.y};
+    two   = (Vector2Int){(two.x+2)-stable.x, (two.y+2)-stable.y};
+    three = (Vector2Int){(three.x+2)-stable.x, (three.y+2)-stable.y};
+    // Insert values into matrix
+    rotMat[one.y][one.x]     = controlledTetromino.blocks[1];
+    rotMat[two.y][two.x]     = controlledTetromino.blocks[2];
+    rotMat[three.y][three.x] = controlledTetromino.blocks[3];
+    // Rotate based on predefined matrix
+    if(leftRight)
+    {
+        for (uInt i = 0; i < 5; i++)
+        {
+            for (uInt j = 0; j < 5; j++)
+            {
+                rotMat[i][j].x += LRotMat[i][j].x;
+                rotMat[i][j].y += LRotMat[i][j].y;
+            }
+        }
+    }
+    else
+    {
+        for (uInt i = 0; i < 5; i++)
+        {
+            for (uInt j = 0; j < 5; j++)
+            {
+                rotMat[i][j].x += RRotMat[i][j].x;
+                rotMat[i][j].y += RRotMat[i][j].y;
+            }
+        }
+    }
+    // Assign new values
+    controlledTetromino.blocks[1] = rotMat[one.y][one.x];
+    controlledTetromino.blocks[2] = rotMat[two.y][two.x];
+    controlledTetromino.blocks[3] = rotMat[three.y][three.x];
+}
+
+static bool AttemptRotateTetromino(bool leftRight)
+{
+    Tetromino before = controlledTetromino;
+    RemoveTetrominoFromGrid();
+    RotateTetromino(leftRight);
+    if (CheckValidTetromino())
+    {
+        AddTetrominoToGrid();
+        return true;
+    }
+    else
+    {
+        RotateTetromino(!leftRight);
+        AddTetrominoToGrid();
+        return false;
+    }
+}
+
+
+static void HandlePlayerInput()
+{
+    if (IsKeyPressed(KEY_RIGHT))
+    {
+        AttemptMoveTetromino(1, 0);
+    }
+    if (IsKeyPressed(KEY_LEFT))
+    {
+        AttemptMoveTetromino(-1, 0);
+    }
+    if (IsKeyPressed(KEY_DOWN))
+    {
+        if(!AttemptMoveTetromino(0, 1))
+        {
+            SpawnTetromino();
+        }
+        tickCount = 0;
+    }
+    if (IsKeyPressed(KEY_COMMA) && controlledTetromino.type != Box)
+    {
+        // Rotate Left
+        AttemptRotateTetromino(true);
+    }
+    if (IsKeyPressed(KEY_PERIOD) && controlledTetromino.type != Box)
+    {
+        // Rotate Right
+        AttemptRotateTetromino(false);
+    }
+}
+
+
+
+static void DeleteLinesAndScore(uInt linesDeleted)
+{
+    // Delete Lines
+    if (linesDeleted == 0){return;}
+    for (uInt i = 0; i < linesDeleted; i++)
+    {
+        uInt line = deletedLines[i];
+        for (uInt j = 0; j < GRIDXMAX; j++)
+        {
+            uInt Pos = (line * GRIDXMAX) + j;
+            grid[Pos].filled = false;
+        }
+    }
+    // Move everything down by lines deleted
+    uInt line = deletedLines[linesDeleted-1]-1;
+    for(int i = line; i >= 0; i--)
+    {
+        line = i;
+        for (uInt j = 0; j < GRIDXMAX; j++)
+        {
+            uInt Pos = (line * GRIDXMAX) + j;
+            uInt nPos = ((line+linesDeleted) * GRIDXMAX) + j;
+            grid[nPos].filled = grid[Pos].filled;
+            grid[Pos].filled = false;
+        }
+    }
+}
+
+
+static void CheckLineDelete()
+{
+    uInt linesDeleted = 0;
+    deletedLines[0] = 0;
+    deletedLines[1] = 0;
+    deletedLines[2] = 0;
+    deletedLines[3] = 0;
+    uInt maxCheck = 0;
+    for (int i = GRIDYMAX; i >= 0; i--)
+    {
+        bool delete = true;
+        for (uInt j = 0; j < GRIDXMAX; j++)
+        {
+            uInt Pos = (i * GRIDXMAX) + j;
+            if(!grid[Pos].filled){delete = false;}
+        }
+        if(delete)
+        {
+            deletedLines[linesDeleted] = i;
+            linesDeleted++;
+        }
+        if(linesDeleted > 0){maxCheck++;}
+        if(maxCheck >= 4){break;}
+    }
+    if (deletedLines > 0)
+    {
+        DeleteLinesAndScore(linesDeleted);
+    }
     
 }
 
@@ -117,24 +354,16 @@ int main()
     while(!WindowShouldClose())
     {
         if (((state & PAUSED) == PAUSED) || ((state & GAME_OVER) == GAME_OVER))
-        {continue;}
+        {
+            BeginDrawing();
+            EndDrawing();
+            continue;
+        }
         tickCount += GetFrameTime();
+        HandlePlayerInput();
         if (tickCount >= tickSpeed)
         {
-            RemoveTetrominoFromGrid();
-            MoveTetrominoRelative(0, 1);
-            if (CheckValidTetromino())
-            {
-                AddTetrominoToGrid();
-            }
-            else
-            {
-                MoveTetrominoRelative(0, -1);
-                // Put down block and spawn new Tetromino
-                AddTetrominoToGrid();
-                SpawnTetromino();
-            }
-            tickCount = 0;
+            GameUpdate();
         }
         BeginDrawing();
             ClearBackground(BLACK);
